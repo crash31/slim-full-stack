4ee28f126a6e4c780086b422264c0b22b2cb7538
Kyle Rodgers <kyler@KyleR-iMac.local>
2017-11-08 17:15:12 -0500

    Started integrating Webpack. I want to be able to use my fun front-end technologies. It's super simple right now, but will scale up.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+
+let config = {
+  entry: './assets/javascript/index.js',
+  output: {
+    filename: './public/resources/js/output.js'
+  }
+}
+
+module.exports = config;
