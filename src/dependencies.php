21635bae65ccd6a01b74820d20b6a168d7c251a3
Kyle Rodgers <kyler@KyleR-iMac.local>
2017-11-08 15:38:13 -0500

    Ok, calm down. Twig has been integrated and is working fairly well. We'll see if this was worth it down the line, but I personally had fun doing it.

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,6 +3,20 @@
 
 $container = $app->getContainer();
 
+// Register Twig View helper
+$container['view'] = function ($c) {
+  $settings = $c->get('settings')['renderer'];
+  $view = new \Slim\Views\Twig($settings['template_path'], [
+    'cache' => false
+  ]);
+
+  // Instantiate and add Slim specific extension
+  $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+  $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+  return $view;
+};
+
 // view renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,19 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+// Define named route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+  return $this->view->render($response, 'profile.html', [
+    'name' => $args['name']
+  ]);
+})->setName('profile');
+
+// Render from string
+$app->get('/hi/{name}', function ($request, $response, $args) {
+  $str = $this->view->fetchFromString('<p>Hi, my name is {{ name }}.</p>', [
+    'name' => $args['name']
+  ]);
+  $response->getBody()->write($str);
+  return $response;
+});
