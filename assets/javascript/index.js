d1af76607044b3233f11496b8eb8c373ca9e1c58
Kyle Rodgers <kyler@KyleR-iMac.local>
2017-11-13 09:53:03 -0500

    Further configuration of Webpack for more robust front-end development. I have to admit, I am a bit addicted to the front-end.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,13 @@
-console.log('Webpack is up and running!');
+// Add Styling to the project
+require('../scss/styles.scss');
+
+class Log {
+  writeLog() {
+    console.log("Webpack is up and running!");
+    console.log("Babel is up and running es2015!");
+  }
+}
+
+const myLog = new Log;
+
+myLog.writeLog();
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,19 @@ let config = {
   entry: './assets/javascript/index.js',
   output: {
     filename: './public/resources/js/output.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node_modules directory
+        loader: "babel-loader" // use this (babel-core) loader
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
   }
 }
 
