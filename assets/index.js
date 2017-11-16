2c7431b587bfb0722f2152c9dbe3944d6aa90575
Kyle Rodgers <kyler@KyleR-iMac.local>
2017-11-16 10:57:01 -0500

    Added better JS class organization.

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,12 @@
+// Import JS Files
+// Classes
+import Log from './javascript/classes/log'
+
 // Add Styling to the project
 import 'bootstrap'
 
+// Add custom SCSS
 require('./scss/styles.scss')
-
-class Log {
-  writeLog () {
-    console.log('Webpack is up and running!')
-    console.log('But is the Webpack Dev Server?')
-    console.log('BrowserSync is up and running!')
-  }
-}
 
 const myLog = new Log()
 
---
+++ assets/javascript/classes/log.js
@@ -0,0 +1,9 @@
+class Log {
+  writeLog () {
+    console.log('Webpack is up and running!')
+    console.log('But is the Webpack Dev Server?')
+    console.log('BrowserSync is up and running!')
+  }
+}
+
+export default Log
